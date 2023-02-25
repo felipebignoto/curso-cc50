36277f92e06683003642bcbd67a692e8daa449f1
Felipe-Bignoto <felipebignoto@gmail.com>
2023-02-25 19:25:39 -0300

    minuto: 1:12:00

--- c/hello.c
+++ c/hello.c
@@ -1,12 +1,37 @@
+/*Estudos da linguagem C
+
+*printf
+*scanf
+*comentarios
+*comandos:
+    *rm - remover
+    *mv - mover / renomear
+    *mkdir nome - cria uma pasta
+*tipos de valores/variaveis:
+    *string -%s
+    *bool
+    *char -%c
+    *double
+    *float -%f
+    *int - %i
+    *long -%li
+*operadore:
+    *soma (+)
+    *subtração (-)
+    *multiplicaçõa (*)
+    *divisão (/)
+    *resto (%)
+*/
 #include <stdio.h>
 
 int main(){
     printf("Digite seu nome: ");
     char nome[100];
-    scanf("%s",nome);
+    scanf("%s",&nome);
     printf("Ola, %s ! ", nome);
     //Comentario na linha
     /*
     Linhas de comentarios
     */
+   
 }
---
+++ c/operadores.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+   
+   int a,b;
+   printf("Digite um numero inteiro: ");
+   scanf("%i",&a);
+   printf("Digite outro numero inteiro: ");
+   scanf("%i",&b);
+   printf("%i + %i = %i",a,b,a+b);
+   
+}
